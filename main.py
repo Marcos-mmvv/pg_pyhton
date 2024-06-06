d274b8da335a94d4130b896c214f707c181a44c3
marcos-mmvv <marcosveras.mv@hotmail.com>
2024-06-05 21:39:00 -0300

    Versão 1.0

---
+++ main.py
@@ -0,0 +1,12 @@
+# Progressão Geométrica
+
+#Função
+calcular_pg = lambda x: x * 2
+
+#Lista númerica
+numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+#Exibe o resultado PG
+print(F'{'-'*20} PROGRESSÃO GEOMÉTRICA {'-'*20} \n')
+print(list(map(calcular_pg, numeros)))
+
